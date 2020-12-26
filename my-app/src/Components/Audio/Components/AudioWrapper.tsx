f4fb1baad404ed010868a757aeceb823e820387e
Andrey <andreylytovchenko@icloud.com>
2020-12-26 21:16:51 +0300

    design of audio

--- my-app/src/Components/Audio/Components/AudioWrapper.tsx
+++ my-app/src/Components/Audio/Components/AudioWrapper.tsx
@@ -16,16 +16,25 @@ export const AudioWrapper = (props: AudioWrapperProps) => {
             <h3>{props.data.name}</h3>
             <div className="flex">
                 <div className="plus" onClick={() => props.handlers(props.data.plus)}>
-                    <h4>Плюс</h4>
-                    <i className="fas fa-music"></i>
-                    <button type="button">Слушать</button>
-                    <a href="" download>Скачать</a>
+                    <div className="labels">
+                        <h4>Плюс</h4>
+                        <i className="fas fa-music"></i>
+                    </div>
+                    <div className="buttons">
+                        <button type="button">Слушать</button>
+                        <a href="" download>Скачать</a>
+                    </div>
+
                 </div>
                 <div className="minus" onClick={() => props.handlers(props.data.minus)}>
-                    <h4>Минус</h4>
-                    <i className="fas fa-drum"></i>
-                    <button type="button">Слушать</button>
-                    <a href="" download>Скачать</a>
+                    <div className="labels">
+                        <h4>Минус</h4>
+                        <i className="fas fa-drum"></i>
+                    </div>
+                    <div className="buttons">
+                        <button type="button">Слушать</button>
+                        <a href="" download>Скачать</a>
+                    </div>
                 </div>
             </div>
         </div>
